d292abbf8442074fcba3a9b80211a0b3e135b70a
reminon <reminon@gmail.com>
2025-12-28 12:11:23 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,9 +89,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 TW_THEME := portrait_hdpi
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := boot system vendor
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-TARGET_NO_RECOVERY := true
-BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2025-02-01
